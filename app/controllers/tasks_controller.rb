2c2b98b87d0293b8a1c6bf0e016d5af868611ece
tsusuga <tsusuga@j-com.co.jp>
2022-01-22 08:05:43 +0900

    編集機能実装、パーシャル導入

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,6 +12,7 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def create
@@ -20,6 +21,12 @@ class TasksController < ApplicationController
     redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
   end
 
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
+
+  end
   private
 
   def task_params
