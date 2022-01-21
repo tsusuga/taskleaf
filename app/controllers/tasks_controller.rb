711dee0dcdddd824ae86333cecb2b055adc69f09
tsusuga <tsusuga@j-com.co.jp>
2022-01-22 08:31:55 +0900

    削除機能実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -25,8 +25,14 @@ class TasksController < ApplicationController
     task = Task.find(params[:id])
     task.update!(task_params)
     redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
-
   end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
+  end
+
   private
 
   def task_params
