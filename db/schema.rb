f4fa4672b0de902dd6adae67a1f27ce1aa2a71a7
tsusuga <tsusuga@j-com.co.jp>
2022-01-27 08:13:46 +0900

    adminユーザ登録機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_25_132352) do
+ActiveRecord::Schema.define(version: 2022_01_26_222139) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2022_01_25_132352) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
