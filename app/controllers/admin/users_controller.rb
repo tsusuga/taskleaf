f6a1a3f7e45ed902dd4811e46d7a66d4387233b7
tsusuga <tsusuga@j-com.co.jp>
2022-01-29 16:49:08 +0900

    admin機能実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,8 +1,19 @@
 class Admin::UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+
+  end
   def new
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
   def create
     @user = User.new(user_params)
     if @user.save
@@ -12,15 +23,21 @@ class Admin::UsersController < ApplicationController
     end
   end
 
-  def edit
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」を更新しました。"
+    else
+      render :edit
+    end
   end
 
-  def show
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザー「#{@user.name}」を削除しました。"
   end
-
-  def index
-  end
-
   private
 
   def user_params
