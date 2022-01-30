b35c3ffa2e987d113d6d9de7c874ec58748fb54a
tsusuga <tsusuga@j-com.co.jp>
2022-01-30 17:15:04 +0900

    DRYに

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,9 @@
 class TasksController < ApplicationController
-
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
-    @tasks = current_user.tasks
   end
 
   def show
-    @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -13,7 +11,6 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = current_user.tasks.find(params[:id])
   end
 
   def create
@@ -26,14 +23,12 @@ class TasksController < ApplicationController
   end
 
   def update
-    task = current_user.tasks.find(params[:id])
-    task.update!(task_params)
+    @task.update!(task_params)
     redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
   end
 
   def destroy
-    task = current_user.tasks.find(params[:id])
-    task.destroy
+    @task.destroy
     redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
   end
 
@@ -41,5 +36,9 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:name, :description, :priority)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
   end
 end
