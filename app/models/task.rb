76c36a5ba4487c8fa789dfd275e405432e0e6ea1
tsusuga <tsusuga@j-com.co.jp>
2022-01-30 11:35:44 +0900

    UserとTaskの関連付け

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,8 @@ class Task < ApplicationRecord
   validates :name, length: {maximum: 30}
   validate :validate_name_not_including_comma
 
+  belong_to :user
+
   private
 
   def validate_name_not_including_comma
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_26_222139) do
+ActiveRecord::Schema.define(version: 2022_01_30_021052) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", null: false
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2022_01_26_222139) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "priority"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
