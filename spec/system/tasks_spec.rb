03a7b2129bd4358d8d95cd7f484f6dcacb19f1a8
tsusuga <tsusuga@j-com.co.jp>
2022-02-06 16:16:09 +0900

    spec作成

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -3,17 +3,24 @@ require 'rails_helper'
 describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     before do
-      # ユーザAを作成しておく
-      # 作成者がユーザAであるタスクを作成しておく
+      #ユーザを作成しておく
+      user_a = FactoryBot.create(:user, name: 'ユーザA', email: 'a@example.com')
+      #作者がユーザAであるタスクを作成しておく
+      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
     end
 
     context 'ユーザAがログインしているとき' do
       before do
-        # ユーザAでログインする
+        #ユーザAでログインする
+        visit login_path
+        fill_in 'メールアドレス', with: 'a@example.com'
+        fill_in 'パスワード', with: 'password'
+        click_button 'ログインする'
       end
 
       it 'ユーザAが作成したタスクが表示される' do
         # 作成済みのタスクの名称が画面上に表示されていることを確認
+        expect(page).to have_content '最初のタスク'
       end
     end
   end
