78b789bb78c88f676b6e547f43721cf8b25978d5
tsusuga <tsusuga@j-com.co.jp>
2022-02-06 19:37:58 +0900

    詳細表示機能のSpec追加

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
+  let(:user_a) {FactoryBot.create(:user, name: 'ユーザA', email: 'a@example.com')}
+  let(:user_b) {FactoryBot.create(:user, name: 'ユーザB', email: 'b@example.com')}
+  let!(:task_a) {FactoryBot.create(:task, name: '最初のタスク', user: user_a)}
+
+  before do
+    visit login_path
+    fill_in 'メールアドレス', with: login_user.email
+    fill_in 'パスワード', with: login_user.password
+    click_button 'ログインする'
+  end
+
   describe '一覧表示機能' do
-    let(:user_a) {FactoryBot.create(:user, name: 'ユーザA', email: 'a@example.com')}
-    let(:user_b) {FactoryBot.create(:user, name: 'ユーザB', email: 'b@example.com')}
-
-    before do
-      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
-      visit login_path
-      fill_in 'メールアドレス', with: login_user.email
-      fill_in 'パスワード', with: login_user.password
-      click_button 'ログインする'
-    end
-
     context 'ユーザAがログインしているとき' do
       let(:login_user) {user_a}
 
@@ -27,6 +27,20 @@ describe 'タスク管理機能', type: :system do
       it 'ユーザAが作成したタスクが表示されない' do
         # ユーザAが作成したタスクの名称が画面上に表示されないことを確認
         expect(page).to have_no_content '最初のタスク'
+      end
+    end
+  end
+
+  describe '詳細表示機能' do
+    context 'ユーザAがログインしているとき' do
+      let(:login_user) {user_a}
+
+      before do
+        visit task_path(task_a)
+      end
+
+      it 'ユーザAが作成したタスクが表示される' do
+        expect(page).to have_content '最初のタスク'
       end
     end
   end
